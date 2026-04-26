1c4444512526c34100cda24354de0dd571c040a2
seungryeol <seungryeol156@gmail.com>
2026-04-26 12:55:50 +0900

    part7

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # 추가 input 으로 표시 되는 Choice 개수
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fieldset 나 fields 값을 바꿔서 순서 지정 가능
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    # inlines 로 `ForeignKey` 관계를 여러개 등록할 수 있습니다.
+    inlines = [ChoiceInline]
+
+    # Question 모델의 was_published_recently()도 포함 시킬 수 있음
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+# 모델과 admin_class 연결
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,6 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # boolean=True이면 Django admin에서 아이콘(체크/엑스)으로 표시됨
+    # ordering은 정렬 기준 필드, description은 목록 컬럼 헤더 텍스트
+    @admin.display(boolean=True, ordering="pub_date", description="Published recently")
     def was_published_recently(self):
         """
             최근 글은 오늘 기준으로 하루 간격
